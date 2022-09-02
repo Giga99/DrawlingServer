05f19d33696116a16863470248f4455bbb1883bd
Giga99 <igorstevanovic99@gmail.com>
2022-09-02 02:21:22 +0200

    Port change

--- src/main/kotlin/com/doodlekong/Application.kt
+++ src/main/kotlin/com/doodlekong/Application.kt
@@ -9,7 +9,7 @@ val server = DrawingServer()
 val gson = Gson()
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = 8000, host = "0.0.0.0") {
         configureSerialization()
         configureSockets()
         configureMonitoring()
