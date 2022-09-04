80ca987d2108f330807353ca14cacb3ca3e09d86
Giga99 <igorstevanovic99@gmail.com>
2022-09-04 22:17:02 +0200

    Added home route

--- src/main/kotlin/com/doodlekong/plugins/Routing.kt
+++ src/main/kotlin/com/doodlekong/plugins/Routing.kt
@@ -1,13 +1,24 @@
 package com.doodlekong.plugins
 
+import com.doodlekong.data.models.BasicApiResponse
 import com.doodlekong.routes.roomRoutes
 import com.doodlekong.routes.webSocketRoutes
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(Routing) {
         roomRoutes()
         webSocketRoutes()
+    }
+}
+
+fun Route.homeRoute() {
+    route("/") {
+        get {
+            call.respond(HttpStatusCode.OK, BasicApiResponse(successful = true, message = "Server is running!"))
+        }
     }
 }
