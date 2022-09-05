7b3bfa1f9d42bd1c675c8f7f06b3fe007e5edf8d
Giga99 <igorstevanovic99@gmail.com>
2022-09-05 23:15:07 +0200

    Added serbian wordlist

--- src/main/kotlin/com/doodlekong/data/WordList.kt
+++ src/main/kotlin/com/doodlekong/data/WordList.kt
@@ -2,11 +2,13 @@ package com.doodlekong.data
 
 enum class WordList {
     PROGRAMMERS_WORDLIST,
+    SERBIAN_WORDLIST,
     DEFAULT_WORDLIST;
 
     fun toFileName() =
         when (this) {
             PROGRAMMERS_WORDLIST -> "programmers_wordlist.txt"
+            SERBIAN_WORDLIST -> "serbian_wordlist.txt"
             else -> "default_wordlist.txt"
         }
 
